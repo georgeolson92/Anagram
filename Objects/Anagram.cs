e8f25f0674d66e4b118897973b4a4b7c97f71a5e
George Olson and Joel Delight <george+joel@>
2016-07-05 14:17:14 -0700

    Check whether a word is a anagram

--- Objects/Anagram.cs
+++ Objects/Anagram.cs
@@ -7,11 +7,24 @@ namespace Anagram.Objects
   {
     public List<string> AnagramChecker(string input, List<string> wordsToCheck)
     {
+      List<string> results = new List<string> {};
+      char[] inputArray = input.ToCharArray();
+      Array.Sort(inputArray);
+      string sortedInput = string.Join("", inputArray);
+
+
       foreach (string word in wordsToCheck)
       {
+        char[] wordArray = word.ToCharArray();
+        Array.Sort(wordArray);
+        string sortedWord = string.Join("", wordArray);
 
+        if (sortedWord == sortedInput)
+        {
+          results.Add(word);
+        }
       }
-      return wordsToCheck;
+      return results;
     }
   }
 }
